07e67e3be6fcf8c1957e37850aaad6f1b41cdd6f
harshgoel05 <harshgoel05@gmail.com>
2021-10-29 22:26:31 +0530

    ✨ Add authentication middleware

---
+++ api/auth/auth-service.ts
@@ -0,0 +1,5 @@
+import jwt from "jsonwebtoken";
+
+export async function findUserProfile(token: string) {
+  return await jwt.verify(token, process.env.JWT_SECRET || "");
+}
---
+++ api/auth/auth.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from "express";
+import { findUserProfile } from "./auth-service";
+import { SERVER_ERROR, UNAUTHORIZED_ACCESS } from "../util/errors";
+export function authenticate() {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const header = req.headers["authorization"];
+      if (!header) {
+        throw { code: 401, message: UNAUTHORIZED_ACCESS };
+      }
+      const token = header.split(" ")[1];
+      if (!token) {
+        throw { code: 401, message: UNAUTHORIZED_ACCESS };
+      }
+      const user = await findUserProfile(token);
+      res.locals.user = user;
+      next();
+    } catch (err) {
+      if (err.code || err.message) {
+        return res.status(401).json(err.message);
+      }
+      return res.status(500).json(SERVER_ERROR);
+    }
+  };
+}
