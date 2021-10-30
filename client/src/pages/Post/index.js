8c9eca9eee9220dacdeac95b190faf2694998046
ayezinzu <59067019+ayezinzu@users.noreply.github.com>
2021-10-30 19:59:46 +0530

    :lipstick: Add Rightbar to Post page

--- client/src/pages/Post/index.js
+++ client/src/pages/Post/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../../shared/components/Navbar/navbar";
 import Search from "../../shared/components/Search";
 import CreatePost from "../../shared/components/CreatePost";
+import Rightbar from "../../shared/components/Rightbar";
 
 const Post = () => {
   return (
@@ -12,7 +13,7 @@ const Post = () => {
         <CreatePost />
       </div>
 
-      <div className="h-screen w-2/4 bg-gray-50">right bar</div>
+      <Rightbar />
     </div>
   );
 };
